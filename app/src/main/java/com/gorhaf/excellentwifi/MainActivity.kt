d037e39bec0b448b19a7ab2c03daa412b9e053aa
scp <15335747148@163.com>
2025-08-17 12:30:47 +0800

    opt(App): 默认界面为蓝牙界面
    1.默认界面为蓝牙界面。
    
    PCode: main

--- app/src/main/java/com/gorhaf/excellentwifi/MainActivity.kt
+++ app/src/main/java/com/gorhaf/excellentwifi/MainActivity.kt
@@ -2,28 +2,32 @@ package com.gorhaf.excellentwifi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import com.gorhaf.excellentwifi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val bluetoothFragment = BluetoothFragment()
+    private val wifiFragment = WifiFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Load the default fragment
-        loadFragment(WifiFragment())
+        loadFragment(bluetoothFragment)
         binding.bottomNavigation.selectedItemId = R.id.nav_bluetooth
 
         binding.bottomNavigation.setOnItemSelectedListener { item ->
             val selectedFragment: Fragment = when (item.itemId) {
-                R.id.nav_bluetooth -> BluetoothFragment()
-                R.id.nav_wifi -> WifiFragment()
-                else -> BluetoothFragment()
+                R.id.nav_bluetooth -> bluetoothFragment
+                R.id.nav_wifi -> wifiFragment
+                else -> bluetoothFragment
             }
             loadFragment(selectedFragment)
             true
@@ -43,6 +47,7 @@ class MainActivity : AppCompatActivity() {
     external fun stringFromJNI(): String
 
     companion object {
+        private const val TAG = "MainActivity"
         // Used to load the 'excellentwifi' library on application startup.
         init {
             System.loadLibrary("excellentwifi")
