b690628e75c61b789245450623e847187ed96409
scp <15335747148@163.com>
2025-08-31 00:09:46 +0800

    opt(App): 修复BluetoothViewModel里bluetoothAdapter不为空书写错误
    1.将 bluetoothAdapter? 改为 bluetoothAdapter!!。
    
    PCode: main

--- app/src/main/java/com/gorhaf/excellentwifi/mvi/bt/BluetoothActivity.kt
+++ app/src/main/java/com/gorhaf/excellentwifi/mvi/bt/BluetoothActivity.kt
@@ -6,7 +6,6 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
